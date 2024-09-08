c41f514f048a952324651e59b422dbe92c5198d5
Emmanuel Ong <156247276+Emmauwa@users.noreply.github.com>
2024-09-08 16:17:23 +0100

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,15 @@
+# Variables to store personal information
+name = "Emmanuel"
+age = 18
+city = "Bradford"
+hobby = "Content Creation"
+
+# Display the personal information card
+print("==================================")
+print("       Personal Information")
+print("==================================")
+print("name: ", name)
+print("age: ", age)
+print("city: ", city)
+print("hobby: ", hobby)
+print("==================================")
